67000e88f1f3bdf44a18dd91993271484bfcc2fa
MatheusMiranda <mpssm@cesar.org.br>
2024-05-28 15:49:29 -0300

    chore: add springdoc-openapi library

--- api/api/src/main/java/med/voll/api/infra/security/SecurityConfigurations.java
+++ api/api/src/main/java/med/voll/api/infra/security/SecurityConfigurations.java
@@ -28,6 +28,7 @@ public class SecurityConfigurations {
                 .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(req ->{
                     req.requestMatchers(HttpMethod.POST,"/login").permitAll();
+                    req.requestMatchers("/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**").permitAll();
                     //req.requestMatchers(HttpMethod.DELETE,"/medicos").hasRole("ADMIN");
                     req.anyRequest().authenticated();
                 })
